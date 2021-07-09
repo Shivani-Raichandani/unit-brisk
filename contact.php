ec345d849cd59cb89a8ed992c3954d4f0ca6b3eb
Saumya Srishti <saumyasrishti2000@gmail.com>
2021-07-09 16:47:26 +0530

    contact section commit

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+    $to_email="saumyasrishti2000@gmail.com";//director@bluoceanstudios.com";
+    $subject=" Cell Kinectics Contact Messages";
+    $firstname=$_POST['fname'];
+    $lastname=$_POST['lname'];
+    $email=$_POST['email'];
+    $mobile=$_POST['mobno'];
+    $msg=$_POST['ques'];
+    $body="Hello sir..I am $firstname $lastname,\nQuestions/Comments: $msg\n$email\n$mobile";
+    $headers="From: $email";
+    if(mail($to_email,$subject,$body,$headers))
+    {
+        echo '<script type="text/javascript"> ';
+        echo '  if (confirm("Message successfully sent")) {';
+        echo '    document.location.href = "index.html"';
+        echo '  }';
+        echo '</script>';
+        
+    }
+    else
+    {
+        echo "Email sending failed...";
+    }
+
+?>
